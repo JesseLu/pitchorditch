f9c276df5d3ad2d569f50758281bffda7d4c89a1
Jesse Lu <mr.jesselu@gmail.com>
2011-12-30 20:29:41 -0800

    disqus works.

--- main.py
+++ main.py
@@ -19,20 +19,35 @@ class MainPage(webapp.RequestHandler):
 
 class Redirect(webapp.RequestHandler):
     def post(self):
-        self.response.out.write('<html><body>You wrote:<pre>')
-        self.response.out.write(cgi.escape(self.request.get('deckurl')))
-        self.response.out.write(cgi.escape(self.request.url))
-        self.response.out.write('</pre></body></html>')
         str = cgi.escape(self.request.get('deckurl')).split('?id=')
-
         self.redirect('/b?id=' + str[1])
-        # self.redirect('/id')
 
 class Submission(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('<html><body>You wrote:<pre>')
-        self.response.out.write(cgi.escape(self.request.url))
-        self.response.out.write('</pre></body></html>')
+        deck = cgi.escape(self.request.url).split('?id=')[1]
+        self.response.out.write('<html><body>')
+        self.response.out.write("""
+            <iframe src="https://docs.google.com/present/embed?id=%s" 
+                frameborder="0" width="410" height="342">
+            </iframe>
+            """ % deck)
+        self.response.out.write("""
+            <div id="disqus_thread"></div>
+            <script type="text/javascript">
+                /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                var disqus_shortname = 'pitch-ditch'; // required: replace example with your forum shortname
+
+                /* * * DON'T EDIT BELOW THIS LINE * * */
+                (function() {
+                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+            <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+            """)
+        self.response.out.write('</body></html>')
 
         
 
@@ -45,5 +60,5 @@ application = webapp.WSGIApplication(
 def main():
     run_wsgi_app(application)
 
-if __name__ == "__main__":
-    main()
+if __name__ 	== "__main__":
+    main()	
